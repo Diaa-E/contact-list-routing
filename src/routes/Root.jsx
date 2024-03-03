a9edb4e5087ee478662f0abee6466b53ec7b7edc
Diaa-E <blarb21@yahoo.com>
2024-03-03 23:27:29 +0200

    Fix flipped contact ids (copy/paste from tutorial)

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,49 +1,47 @@
 import { Link, Outlet } from "react-router-dom";
 
-const Root = ({ }) => {
-
+export default function Root() {
     return (
-        <>
-            <div id="sidebar">
-                <h1>React Router Contacts</h1>
-                <div>
-                    <form id="search-form" role="search">
-                        <input
-                            aria-label="search contact"
-                            placeholder="Search"
-                            type="search"
-                            id="q"
-                        />
-                        <div
-                            id="search-spinner"
-                            aria-hidden
-                            hidden={true}
-                        ></div>
-                        <div
-                            className="sr-only"
-                            aria-live="polite"
-                        ></div>
-                    </form>
-                    <form method="post">
-                        <button type="submit">New</button>
-                    </form>
-                </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="contact/1">Your name</Link>
-                        </li>
-                        <li>
-                            <Link to="contacts/2">Your friend</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <div id="detail">
-                <Outlet />
-            </div>
-        </>
-    )
-}
-
-export default Root;
+      <>
+        <div id="sidebar">
+          <h1>React Router Contacts</h1>
+          <div>
+            <form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div
+                id="search-spinner"
+                aria-hidden
+                hidden={true}
+              />
+              <div
+                className="sr-only"
+                aria-live="polite"
+              ></div>
+            </form>
+            <form method="post">
+              <button type="submit">New</button>
+            </form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <Link to={`/contacts/1`}>Your Name</Link>
+              </li>
+              <li>
+                <Link to={`/contacts/2`}>Your Friend</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div id="detail">
+            <Outlet />
+        </div>
+      </>
+    );
+  }
