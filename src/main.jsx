60077eb6e24f6b9a443ca0a6ab918ee4288de2bb
Diaa-E <blarb21@yahoo.com>
2024-03-03 23:19:13 +0200

    Add nested route for contact

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,20 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/Root';
 import ErrorPage from './ErrorPage';
+import Contact from './routes/Contact';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />
-  }
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,3 +1,5 @@
+import { Link, Outlet } from "react-router-dom";
+
 const Root = ({ }) => {
 
     return (
@@ -29,15 +31,17 @@ const Root = ({ }) => {
                 <nav>
                     <ul>
                         <li>
-                            <a href="contact/1">Your name</a>
+                            <Link to="contact/1">Your name</Link>
                         </li>
                         <li>
-                            <a href="contacts/2">Your friend</a>
+                            <Link to="contacts/2">Your friend</Link>
                         </li>
                     </ul>
                 </nav>
             </div>
-            <div id="detail"></div>
+            <div id="detail">
+                <Outlet />
+            </div>
         </>
     )
 }
